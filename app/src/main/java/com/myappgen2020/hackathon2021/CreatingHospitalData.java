88b5fa1a7a7473d1e50b0d601132cb7bdd03ceee
PoojanSheth29 <poojansheth29@gmail.com>
2021-03-26 22:22:22 +0530

    1stPush

---
+++ app/src/main/java/com/myappgen2020/hackathon2021/CreatingHospitalData.java
@@ -0,0 +1,14 @@
+package com.myappgen2020.hackathon2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreatingHospitalData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creating_hospital_data);
+    }
+}
---
+++ app/src/main/java/com/myappgen2020/hackathon2021/SignUpHospital.java
@@ -0,0 +1,15 @@
+package com.myappgen2020.hackathon2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpHospital extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_hospital);
+
+    }
+}
